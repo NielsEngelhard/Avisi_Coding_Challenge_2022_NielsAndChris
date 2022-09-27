4258df4dd7d1cd561c03f0558e151accbadc0fc3
niels <niels@DESKTOP-E1ETGFO>
2022-09-27 22:17:23 +0200

    machinekamer with PrintRealScenario for correct answer

--- c-sharp/AvisiCodingChallenge/MachineKamerApp/Program.cs
+++ c-sharp/AvisiCodingChallenge/MachineKamerApp/Program.cs
@@ -1,13 +1,17 @@
 ﻿using TestApp;
 
-WireBox wb = new WireBox(7);
+//WireBox wb = new WireBox(7);
 
-//var lol = new int[5] { 1, 2, 3, 4, 5 };
+////var lol = new int[5] { 1, 2, 3, 4, 5 };
 
-//var totalPossibleCombinations = FactorialCalculator.GetFactorialOfNumber(10);
-//Console.WriteLine($"Total possible combinations is {totalPossibleCombinations}");
+////var totalPossibleCombinations = FactorialCalculator.GetFactorialOfNumber(10);
+////Console.WriteLine($"Total possible combinations is {totalPossibleCombinations}");
 
-var possibleCombinations = FactorialCalculator.Permute(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-WireBoxPuzzleSolver solver = new WireBoxPuzzleSolver();
+//var possibleCombinations = FactorialCalculator.Permute(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+//WireBoxPuzzleSolver solver = new WireBoxPuzzleSolver();
 
-solver.TryCombinations(possibleCombinations);
+//solver.TryCombinations(possibleCombinations);
+
+
+WireBox wb = new WireBox(10);
+wb.PrintRealScenario();
